72575a929decf7fe9c6d14cdd80bc15819592708
a <drz31949@outlook.com>
2017-04-24 01:56:10 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  #root to: 'tasks#index'
   root to: 'toppages#index'
   
-  #resources :tasks
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  
 end
+
+  #root to: 'tasks#index'
+  #resources :tasks
